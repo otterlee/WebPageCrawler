a5b47199c6d9abff867eefb5d37736af36ae8ad9
otterlee <21700525@handong.edu>
2018-06-20 02:34:40 +0900

    Second Commit

--- src/main/java/edu/handong/csee/java/bonushw/WebPageCrawler.java
+++ src/main/java/edu/handong/csee/java/bonushw/WebPageCrawler.java
@@ -2,11 +2,12 @@ package edu.handong.csee.java.bonushw;
 
 public class WebPageCrawler {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		CLIOption cliOption = new CLIOption();
 		cliOption.run(args);
 		
 		URLReader urlReader = new URLReader(cliOption.inputURL);
+		urlReader.read();
 		
 		FileWriter fileWriter = new FileWriter(cliOption.outputPath, urlReader.allLines);
 		fileWriter.write();
